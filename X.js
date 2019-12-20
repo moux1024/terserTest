bdb9bbec78cac1ee6062dbd7734ae7f5531569e7
teng.xue <teng.xue@17zuoye.com>
2019-12-20 18:02:16 +0800

    无法阻止terser干掉__PURE__

---
+++ X.js
@@ -0,0 +1,20 @@
+const abc = () => {};
+
+abc();
+
+/*@__PURE__1*/unknownGlobalFunction();
+
+/*@__PURE__1*/new UnknownGlobalClass();
+
+/* foo
+ * @preserve
+ * @fdsfdsf
+ * #__PURE__
+* */
+var x = function(){
+}
+x();
+
+export default /* @preserve #__PURE__
+*/ unknownGlobalFunctioAn();
+export const value = /*foo @__PURE__*/unknownGlobalFunctioBn();
